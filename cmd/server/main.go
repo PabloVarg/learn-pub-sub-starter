52d0875605b2052261de73db7f8071cd9012f2ce
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-08-28 18:58:36 -0600

    feat(server): Connect to rabbitMQ

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	signal.NotifyContext(ctx, os.Interrupt)
+
+	run(ctx, logger)
+}
+
+func run(ctx context.Context, logger *slog.Logger) {
+	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
+	if err != nil {
+		logger.Error("run", "err", err.Error())
+		return
+	}
+	defer conn.Close()
+
+	logger.Info("connected to rabbitmq", "conn", conn.RemoteAddr().String())
+
+	select {
+	case <-ctx.Done():
+	}
 }
